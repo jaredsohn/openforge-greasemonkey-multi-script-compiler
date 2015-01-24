3cc2a1c897bb0de7193ef4fc75f84a4940fa7120
Jared Sohn <jared.sohn@gmail.com>
2015-01-23 16:44:27 -0800

    Support Flix Plus 2.2: options page in chrome://extensions, set a 38-pixel icon

--- _util/fix_manifest.py
+++ _util/fix_manifest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python                                                               
+#!/usr/bin/python
 
 import json
 obj = json.load(open("development/chrome/manifest.json"))
@@ -11,9 +11,11 @@ obj["page_action"] = dict()
 obj["page_action"]["default_popup"] = "src/popup.html"
 obj["page_action"]["default_icon"] = dict()
 obj["page_action"]["default_icon"]["19"] = "src/favicon_19.png"
+obj["page_action"]["default_icon"]["38"] = "src/favicon_19.png"
 obj["page_action"]["default_title"] = obj["name"]
 
 obj["web_accessible_resources"] = ["src/css/*", "src/img/*"]
 
+obj["options_page"] = "src/options.html"
 
 open("development/chrome/manifest.json", "w").write(json.dumps(obj, indent=4,  separators=(",", ": ")))
--- _util/netflix_fix_manifest.py
+++ _util/netflix_fix_manifest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python                                                               
+#!/usr/bin/python
 
 import json
 obj = json.load(open("development/chrome/manifest.json"))
@@ -13,10 +13,6 @@ contentscript["all_frames"] = False;
 contentscript["js"] = [ "forge/app_config.js", "forge/all.js", "src/js/before_images.js"];
 contentscript["css"] = [];
 contentscript["run_at"] = "document_end";
-
 obj["content_scripts"].append(contentscript);
-
-obj["options_page"] = "src/options.html";
-
 
 open("development/chrome/manifest.json", "w").write(json.dumps(obj, indent=4,  separators=(",", ": ")))
