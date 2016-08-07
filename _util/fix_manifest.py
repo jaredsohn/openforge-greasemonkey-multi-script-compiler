c21f4423fd1d39079d47f5cd980da6132a698915
Jared Sohn <jared.sohn@gmail.com>
2016-08-06 18:43:43 -0700

    Remove favicon_19 from manifest (change made May 14 2016)

--- _util/fix_manifest.py
+++ _util/fix_manifest.py
@@ -10,8 +10,7 @@ obj["icons"]["64"] = "src/favicon_64.png"
 obj["page_action"] = dict()
 obj["page_action"]["default_popup"] = "src/popup.html"
 obj["page_action"]["default_icon"] = dict()
-obj["page_action"]["default_icon"]["19"] = "src/favicon_19.png"
-obj["page_action"]["default_icon"]["38"] = "src/favicon_19.png"
+obj["page_action"]["default_icon"]["38"] = "src/favicon_38.png"
 obj["page_action"]["default_title"] = obj["name"]
 
 obj["web_accessible_resources"] = ["src/css/*", "src/img/*"]
